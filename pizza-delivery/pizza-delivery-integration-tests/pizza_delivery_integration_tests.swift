4fa119a3f9a19caa5f60b4316dc471628021f5e4
Igor Karpov <igork.fet@gmail.com>
2019-05-01 14:49:37 +0300

    UI tests target renamed to integration.

--- pizza-delivery/pizza-delivery-integration-tests/pizza_delivery_integration_tests.swift
+++ pizza-delivery/pizza-delivery-integration-tests/pizza_delivery_integration_tests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class pizza_delivery_UI_tests: XCTestCase {
+class pizza_delivery_integration_tests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
