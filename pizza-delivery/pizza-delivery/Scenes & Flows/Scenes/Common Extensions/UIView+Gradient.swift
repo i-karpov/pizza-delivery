3853ed1f6e923d68fb43344f8764401d7a9c26ff
Igor Karpov <igork.fet@gmail.com>
2019-05-07 01:26:19 +0300

    Splash screen UI design implemented.

--- pizza-delivery/pizza-delivery/Scenes & Flows/Scenes/Common Extensions/UIView+Gradient.swift
+++ pizza-delivery/pizza-delivery/Scenes & Flows/Scenes/Common Extensions/UIView+Gradient.swift
@@ -18,4 +18,13 @@ extension UIView {
         gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
         self.layer.insertSublayer(gradient, at: 0)
     }
+    
+    func applyGradientFromTopToBottom(colors: [UIColor]) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colors.map { $0.cgColor }
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+        self.layer.insertSublayer(gradient, at: 0)
+    }
 }
